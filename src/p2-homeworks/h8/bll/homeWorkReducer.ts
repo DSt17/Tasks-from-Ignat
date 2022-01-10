0598112e166d9d0fcbb82c2c3fa5b1e8231248f6
Dmitry Stoyanov <dima3771770@gmail.com>
2022-01-11 00:25:12 +0300

    compl.task && refact.

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,29 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+type actionType = {
+    type: 'check' | 'sort',
+    payload: 'up' | 'down' | number
+}
+
+export const homeWorkReducer = (state: Array<UserType>, action: actionType): Array<UserType> => { // need to fix any
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            if (action.payload === 'up') {
+                return [...state].sort(function (a, b) {
+                    if (a.name > b.name) return 1
+                    if (a.name < b.name) return -1
+                    else return 0
+                })
+            } else return [...state].sort(function (a, b) {
+                if (a.name < b.name) return 1
+                if (a.name > b.name) return -1
+                else return 0
+            })
         }
         case 'check': {
-            // need to fix
-            return state
+            return state.filter(u => u.age >= action.payload)
         }
-        default: return state
+        default:
+            return state
     }
 }
